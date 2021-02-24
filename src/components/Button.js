44dc1af24ce5ad9feea6127b4bdd454efdcf18b8
josilob <bjosilo@gmail.com>
2021-02-24 18:28:10 -0500

    multiple providers & consumers example;

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 export class Button extends Component {
-	static contextType = LanguageContext; // has to be called 'contextType' exactly in order to work
+	// static contextType = LanguageContext; // has to be called 'contextType' exactly in order to work
 	// static keyword means we assign propery to the class itself
 	// it would be the same as typing:
 	// Button.contextType = LanguageContext
+
+	renderSubmit(value) {
+		return value === 'english' ? 'Submit' : 'Voorleggen';
+	}
+
 	render() {
-		const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
-		return <button className='ui button primary'>{text}</button>;
+		// const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+		// return <button className='ui button primary'>{text}</button>;
+		return (
+			<ColorContext.Consumer>
+				{(color) => (
+					<button className={`ui button ${color}`}>
+						<LanguageContext.Consumer>
+							{(value) => this.renderSubmit(value)}
+						</LanguageContext.Consumer>
+					</button>
+				)}
+			</ColorContext.Consumer>
+		);
 	}
 }
 
