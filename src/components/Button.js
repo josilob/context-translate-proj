07112078a372002f51b41fc04199a38030ba6b75
josilob <bjosilo@gmail.com>
2021-02-24 17:29:23 -0500

    adding default value to context and connecting it to components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 export class Button extends Component {
+	static contextType = LanguageContext; // has to be called 'contextType' exactly in order to work
+	// static keyword means we assign propery to the class itself
+	// it would be the same as typing:
+	// Button.contextType = LanguageContext
 	render() {
-		return <button className='ui button primary'>Submit</button>;
+		const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+		return <button className='ui button primary'>{text}</button>;
 	}
 }
 
---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+// create Context Object
+export default React.createContext('english'); // set up english as default value
