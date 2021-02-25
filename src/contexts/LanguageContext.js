1b1cf6d1324193556203b39f208104190559d9c7
josilob <bjosilo@gmail.com>
2021-02-24 21:24:53 -0500

    context way of replacing redux

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,4 +1,25 @@
 import React from 'react';
 
 // create Context Object
-export default React.createContext('english'); // set up english as default value
+// export default React.createContext('english'); // set up english as default value
+
+const Context = React.createContext('english');
+
+export class LanguageStore extends React.Component {
+	state = { language: 'english' };
+
+	onLanguageChange = (language) => {
+		this.setState(language);
+	};
+
+	render() {
+		return (
+			<Context.Provider
+				value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
+				{this.props.children}
+			</Context.Provider>
+		);
+	}
+}
+
+export default Context;
